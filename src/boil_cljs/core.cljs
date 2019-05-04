d352a2ec99e5f4a351a60cb799b3af7df4091949
ParikshithMohan <parikshithmohan@gmail.com>
2019-05-04 17:57:44 +0530

    part 3 : tryong to figure out a way to get key strokes

--- src/boil_cljs/core.cljs
+++ src/boil_cljs/core.cljs
@@ -5,7 +5,7 @@
 
 (println "This text is printed from src/boil_cljs/core.cljs. Go ahead and edit it and see reloading in action.")
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Hello world!" :key ""}))
 (def ene-health (reagent/atom 25))
 (def pla-health (reagent/atom 50))
 (def random-val (reagent/atom 0))
@@ -25,25 +25,39 @@
 (defn bombcheck? [number]    ;;checking for 40% probability of bomb
   (if (< number 40) (dec-atom ene-health 12)))
 
-;;(defn eleminate [gone]
-  ;;(if (< = @ene-health 0)) {:style{:display "hidden"}})
+(defn eleminate [gone]
+  (if (<= gone 0) [{:style {:color "red"}}] ))
+
+(defn key-detect [capture](.-charCode capture))
+(defn cap [e] #(swap! (:key app-state) (key-detect e)))
+(defn f [e] (println (.-charCode e)))
+
 (defn hello-world []
   [:div
   ;;"The atom random-val has value: " @random-val
   ;;[:input {:type "button" :value "Click me!"
   ;;        :on-click #(swap! random-val (rand-int 100))}]
     [:input {:type "button" :value "Keep Going"
-          :on-click #(reset! status-atom "Advancing...")}]
+          :onClick #(reset! status-atom "Advancing...")}]
     [:input {:type "button" :value "Shoot"
-            :on-click #(shotcheck? (rand-int 100))}]
+            :onClick #(shotcheck? (rand-int 100))}]
     [:input {:type "button" :value "Bomb"
-            :on-click #(bombcheck? (rand-int 100))}]
+            :onClick #(bombcheck? (rand-int 100))}]
     [:input {:type "button" :value "Retreat"
-            :on-click #(reset! status-atom "Retreating")}]
-   [:p "ENEMY HEALTH : " @ene-health {:style{:display "hidden"}}]
+             :onClick #(reset! status-atom "Retreating...")}]
+    [:input {:type "text" :value (:key @app-state)
+             :on-key-press (fn [e]
+                             (println "key press" (.-charCode e))
+                             (if (= 13 (.-charCode e))
+                               (println "ENTER")
+                               (println (char (.-charCode e)))))}]
+
+   [:p [eleminate @ene-health] "ENEMY HEALTH : " @ene-health]
    [:p "PLAYER HEALTH : " @pla-health]
-   [:p "Player Status : " @status-atom]]
-  )
+   [:p "Player Status : " @status-atom]
+   [:p (:key @app-state) " keystroke"]
+   [:p (fn [e] (println (key-detect e)))]
+   ])
 
 (defn mount [el]
   (reagent/render-component [hello-world] el))
